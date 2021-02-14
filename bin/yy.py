ccbcd634f8f66a4af7a8fa1b9f0b98bf16a59e6e
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-14 09:55:51 +0100

    Created bin/yy.py

---
+++ bin/yy.py
@@ -0,0 +1,19 @@
+"""yy
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-14"
+
+class yy:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(yy.yy())
+	
+	def yy():
+		"""yy first funtion
+		"""
+		return "yy" #for the test (see test_yy.py)
+		
+if __name__ == "__main__":
+	yy()
